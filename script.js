d200b15a9697ccaae423153d40a5a2816f562763
Pruvost Paul <paul.pruvost.etu@univ-lille.fr>
2025-01-14 22:27:20 +0100

    avancement sur la partie de gauche

---
+++ script.js
@@ -0,0 +1,4 @@
+const maison = document.getElementById("localisation");
+maison.addEventListener("click", () => {
+    window.open("https://www.google.com/maps/place/Comines,+7780+Comines-Warneton/@50.786475,2.9574273,13z/data=!3m1!4b1!4m6!3m5!1s0x47dcd2c09e08e41d:0x6a29c4f1d1b2e0a0!8m2!3d50.7687459!4d2.9997061!16s%2Fg%2F122_s31j?entry=ttu&g_ep=EgoyMDI1MDExMC4wIKXMDSoASAFQAw%3D%3D")
+});
