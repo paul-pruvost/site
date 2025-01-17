c7c407be09ce8e8573178b9b3381c6a3657ea050
Pruvost Paul <paul.pruvost.etu@univ-lille.fr>
2025-01-17 17:40:17 +0100

    add center part and some informations in. Add functionality to display about section and resume section

--- script.js
+++ script.js
@@ -2,3 +2,63 @@ const maison = document.getElementById("localisation");
 maison.addEventListener("click", () => {
     window.open("https://www.google.com/maps/place/Comines,+7780+Comines-Warneton/@50.786475,2.9574273,13z/data=!3m1!4b1!4m6!3m5!1s0x47dcd2c09e08e41d:0x6a29c4f1d1b2e0a0!8m2!3d50.7687459!4d2.9997061!16s%2Fg%2F122_s31j?entry=ttu&g_ep=EgoyMDI1MDExMC4wIKXMDSoASAFQAw%3D%3D")
 });
+
+const mail = document.getElementById("email");
+mail.addEventListener("click", () => {
+    window.open("mailto: paulpruvost26800@gmail.com");
+});
+
+const phone = document.getElementById("telephone");
+phone.addEventListener("click", () => {
+    window.open("tel:07-83-53-64-31");
+});
+
+const buttons = document.querySelectorAll(".button");
+const aboutButton = document.getElementById("first");
+const resumeButton = document.getElementById("second");
+
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        buttons.forEach(btn => {
+            btn.style.backgroundColor = '';
+            btn.style.color = '';
+        });
+
+        button.style.color = 'rgb(245, 180, 2)';
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    const me = document.getElementById("me").childNodes;
+    const resume = document.getElementById("resume").childNodes;
+
+    const showAbout = () => {
+        me.forEach(child => {
+            if (child.nodeType === 1 && child.id != "button"){
+                child.style.visibility = "visible";        
+            }
+        });
+        resume.forEach(child => {
+            if (child.nodeType === 1 && child.id != "button"){
+                child.style.visibility = "hidden";        
+            }
+        })
+    }
+
+    const showResume = () => {
+        me.forEach(child => {
+            if (child.nodeType === 1 && child.id != "button"){
+                child.style.visibility = "hidden";        
+            }
+        });
+        resume.forEach(child => {
+            if (child.nodeType === 1 && child.id != "button"){
+                child.style.visibility = "visible";        
+            }
+        })
+    }
+
+    aboutButton.addEventListener("click", showAbout);
+    resumeButton.addEventListener("click", showResume);
+    aboutButton.click();
+});
